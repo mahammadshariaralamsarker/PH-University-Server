11048577eeb8467884d66a9717ba2c14f5d6f06e
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-31 12:02:43 +0600

    5.28 second for 29.3 video

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -1,29 +1,48 @@
-import { Form, Select } from 'antd';
-import { Controller } from 'react-hook-form';
+import React from "react";
+import { Form, Select } from "antd";
+import { Controller } from "react-hook-form";
 
-type TPHSelectProps = {
-  label: string;
-  name: string;
-  options: { value: string; label: string; disabled?: boolean }[] | undefined;
-  disabled?: boolean;
-  mode?: 'multiple' | undefined;
-};
+const PHSelect: React.FC = ({ label, name }) => {
+  const handleChange = (value: string) => {
+    console.log(`selected Value>> ${value}`);
+  };
 
-const PHSelect = ({ label, name, options, disabled, mode }: TPHSelectProps) => {
   return (
     <Controller
       name={name}
-      render={({ field, fieldState: { error } }) => (
+      render={({ field }) => (
         <Form.Item label={label}>
           <Select
-            mode={mode}
-            style={{ width: '100%' }}
+            style={{ width: 200 }}
+            placeholder="Search to Select"
             {...field}
-            options={options}
-            size="large"
-            disabled={disabled}
+            options={[
+              {
+                value: "Not Identified",
+                label: "Not Identified",
+              },
+              {
+                value: "2",
+                label: "Closed",
+              },
+              {
+                value: "3",
+                label: "Communicated",
+              },
+              {
+                value: "4",
+                label: "Identified",
+              },
+              {
+                value: "5",
+                label: "Resolved",
+              },
+              {
+                value: "6",
+                label: "Cancelled",
+              },
+            ]}
           />
-          {error && <small style={{ color: 'red' }}>{error.message}</small>}
         </Form.Item>
       )}
     />
@@ -31,3 +50,37 @@ const PHSelect = ({ label, name, options, disabled, mode }: TPHSelectProps) => {
 };
 
 export default PHSelect;
+
+// import { Form, Select } from 'antd';
+// import { Controller } from 'react-hook-form';
+
+// type TPHSelectProps = {
+//   label: string;
+//   name: string;
+//   options: { value: string; label: string; disabled?: boolean }[] | undefined;
+//   disabled?: boolean;
+//   mode?: 'multiple' | undefined;
+// };
+
+// const PHSelect = ({ label, name, options, disabled, mode }: TPHSelectProps) => {
+//   return (
+//     <Controller
+//       name={name}
+//       render={({ field, fieldState: { error } }) => (
+//         <Form.Item label={label}>
+//           <Select
+//             mode={mode}
+//             style={{ width: '100%' }}
+//             {...field}
+//             options={options}
+//             size="large"
+//             disabled={disabled}
+//           />
+//           {error && <small style={{ color: 'red' }}>{error.message}</small>}
+//         </Form.Item>
+//       )}
+//     />
+//   );
+// };
+
+// export default PHSelect;
